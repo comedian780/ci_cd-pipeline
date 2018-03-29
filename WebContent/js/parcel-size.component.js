6dcbbf8d248ce5a2a85fad050e0b61c4c1a1d930
comedian780 <aaron.s.luellwitz@stud.hs-kempten.de>
2018-03-29 19:48:18 +0200

    Code für Phase 2.3 inklusive Berechnungsfunktionalität

---
+++ WebContent/js/parcel-size.component.js
@@ -0,0 +1,66 @@
+angular.
+  module('parcelConfig').
+  component('parcelSize', {
+	  transclude: true,
+	  template:'<div class="w3-panel w3-card-2">'+
+      '<div class="w3-container w3-teal">'+
+        '<h2>Paketgröße</h2>'+
+      '</div>'+
+    	'<form class="w3-container">'+
+    '<div class="w3-cell">'+
+    '<label>Länge</label>'+
+    '<input class="w3-input" id="cfg-size-length" type="text" ng-model="$ctrl.parcel.size.length">'+
+    '</div>'+
+    '</br>'+
+    '<div class="w3-cell">'+
+    '<label>Breite</label>'+
+    '<input class="w3-input" id="cfg-size-height" type="text" ng-model="$ctrl.parcel.size.height">'+
+    '</div>'+
+    '</br>'+
+    '<div class="w3-cell">'+
+    '<label>Tiefe</label>'+
+    '<input class="w3-input" id="cfg-size-depth" type="text" ng-model="$ctrl.parcel.size.depth">'+
+    '</div>'+
+    '</br>'+
+    '<div class="w3-button w3-teal" ng-click="$ctrl.calcSize($ctrl.parcel)" style="cursor: pointer;">'+
+      '<h3 class="w3-opacity" >Paketgröße berechnen</h3>'+
+    '</div>'+
+    '<div class="w3-cell" >'+
+      '<label id="cfg-category">Paketgröße: {{$ctrl.parcel.category}}</label>'+
+    '</div>'+
+    '</form>'+
+    '</div>',
+
+
+  controller: function ParcelSiceController($rootScope, $http) {
+
+    this.parcel = {
+      size: {length: '0', height: '0', depth: '0'},
+      category: 'S'
+    };
+
+    $rootScope.parcelsize = this.parcel;
+
+    this.calcSize=function($parcel){
+      pak_cat = "";
+      gurtmass = +$parcel.size.length + 2 * $parcel.size.height + 2 * $parcel.size.depth;
+
+      if(gurtmass >= 80 && gurtmass <= 300){
+        pak_cat = "XL";
+      }else if (gurtmass >= 65 && gurtmass <= 80) {
+        pak_cat = "L";
+      }else if (gurtmass >= 50 && gurtmass <= 65) {
+        pak_cat = "M";
+      }else if (gurtmass >= 35 && gurtmass <= 50) {
+        pak_cat = "S";
+      }else if (gurtmass >= 0 && gurtmass <= 35){
+        pak_cat = "XS";
+      }else{
+        pak_cat = "";
+      }
+
+      $parcel.category = pak_cat;
+    }
+
+  }
+});
