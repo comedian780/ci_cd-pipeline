2b99769e55e9a8e1e2745d7d80e157e52b17e93e
comedian780 <aaron.s.luellwitz@stud.hs-kempten.de>
2018-05-03 13:13:48 +0200

    Datenbankverbindung angepasst Dockerfile mit JDK erstellt

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM alpine:latest
+
+MAINTAINER AaronLuellwitz <aaron.luellwitz@gmx.de>
+
+RUN mkdir APP
+
+COPY . APP/.
+WORKDIR APP
+
+ENV LANG C.UTF-8
+
+# add a simple script that can auto-detect the appropriate JAVA_HOME value
+# based on whether the JDK or only the JRE is installed
+RUN { \
+		echo '#!/bin/sh'; \
+		echo 'set -e'; \
+		echo; \
+		echo 'dirname "$(dirname "$(readlink -f "$(which javac || which java)")")"'; \
+	} > /usr/local/bin/docker-java-home \
+	&& chmod +x /usr/local/bin/docker-java-home
+ENV JAVA_HOME /usr/lib/jvm/java-1.8-openjdk
+ENV PATH $PATH:/usr/lib/jvm/java-1.8-openjdk/jre/bin:/usr/lib/jvm/java-1.8-openjdk/bin
+
+ENV JAVA_VERSION 8u151
+ENV JAVA_ALPINE_VERSION 8.151.12-r0
+
+RUN set -x \
+	&& apk add --no-cache \
+		openjdk8="$JAVA_ALPINE_VERSION" \
+&& [ "$JAVA_HOME" = "$(docker-java-home)" ]
+
+
+# RUN java -jar ./build/libs/ci_cd-pipeline.jar
+RUN ls ./build/libs
--- gradle/src/api/MysqlCon.java
+++ gradle/src/api/MysqlCon.java
@@ -3,25 +3,25 @@ package api;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement; 
+import java.sql.Statement;
 
 public class MysqlCon {
 
-	
-    
- 
-    public String getSize(double gurt){  
-	    try{  
-		    Class.forName("com.mysql.cj.jdbc.Driver");  
-		    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/parcel","parcel","parcelpassword");   
-		    Statement stmt=con.createStatement();  
-		    ResultSet rs=stmt.executeQuery("select distinct size from sizes where min <= " + gurt + " and max >= " + gurt);  
+
+
+
+    public String getSize(double gurt){
+	    try{
+		    Class.forName("com.mysql.cj.jdbc.Driver");
+		    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/parcel?serverTimezone=UTC","parcel","parcelpassword");   
+		    Statement stmt=con.createStatement();
+		    ResultSet rs=stmt.executeQuery("select distinct size from sizes where min <= " + gurt + " and max >= " + gurt);
 		    rs.next();
-		    String size =rs.getString(1);  
-		    con.close();  
+		    String size =rs.getString(1);
+		    con.close();
 		    return size;
-	    }catch(Exception e){ System.out.println(e);} 
+	    }catch(Exception e){ System.out.println(e);}
 	    	return "";
-    }  
-  
+    }
+
 }
