c33d0462426698b1a7016b43d1b0d1fd2f527197
comedian780 <aaron.s.luellwitz@stud.hs-kempten.de>
2018-05-27 18:35:53 +0200

    Port im Startskript korrigiert, Kommentare ins Startskript eingefügt

--- initializeTestServer.sh
+++ initializeTestServer.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-docker-machine create --driver virtualbox --engine-insecure-registry 193.174.205.28:44 parcel-test
+echo "Creating VM"
+docker-machine create --driver virtualbox --engine-insecure-registry 193.174.205.28:443 parcel-test
+echo "Changing settings to VM's docker"
 eval $(docker-machine env parcel-test)
+echo "Creating network for containers"
 docker network create --driver bridge parcelnetwork
+echo "Starting containers"
 docker run -d --restart always --network=parcelnetwork -p 3306:3306 --name=parcel-db 193.174.205.28:443/parcel-db
 docker run -d --restart always --network=parcelnetwork -p 80:80 --name=parcel-frontend 193.174.205.28:443/parcel-frontend
 docker run -d --network=parcelnetwork --name=parcel-webservice -p 8443:8443 193.174.205.28:443/parcel-api ./start.sh
