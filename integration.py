8d1b2949d81dedd3e6fe143944f6c5b5a20b87ea
comedian780 <aaron.s.luellwitz@stud.hs-kempten.de>
2018-06-17 15:38:10 +0200

    URL statt docker-machine Abfrage in Testskripts

--- integration.py
+++ integration.py
@@ -1,7 +1,8 @@
 import requests
 import subprocess
 
-vm_url = str(subprocess.check_output(["docker-machine","url", "parcel-test"])).split(':')[1].split('/')[2]
+#vm_url = str(subprocess.check_output(["docker-machine","url", "parcel-test"])).split(':')[1].split('/')[2]
+vm_url = "www.allgaeu-parcel-service.com"
 
 # Post request to the API
 r = requests.post("http://" + str(vm_url) + ":8443/parcel/size", json={"length": 1, "width": 2, "height": 3, "size": ""})
