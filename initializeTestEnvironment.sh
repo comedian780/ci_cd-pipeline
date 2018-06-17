24995e62183f9fda43853a1f85f5430fa03bd4b0
comedian780 <aaron.s.luellwitz@stud.hs-kempten.de>
2018-06-17 13:01:08 +0200

    Eine VM statt drei

--- initializeTestEnvironment.sh
+++ initializeTestEnvironment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 vm_name="parcel-loadbalancer"
-test_server1_name="parcel-test1"
-test_server2_name="parcel-test2"
-echo "Starting Test VM 1"
-sh initializeVM.sh $test_server1_name
-echo "Starting Test VM 2"
-sh initializeVM.sh $test_server2_name
+#test_server1_name="parcel-test1"
+#test_server2_name="parcel-test2"
+#echo "Starting Test VM 1"
+#sh initializeVM.sh $test_server1_name
+#echo "Starting Test VM 2"
+#sh initializeVM.sh $test_server2_name
 echo "Checking if Loadbalancer exists"
 if(docker-machine ls -q | grep "^$vm_name\$"); then
   if(docker-machine status $vm_name | grep "^Running\$"); then
@@ -17,18 +17,24 @@ fi
 echo "Starting Load Balancer"
 docker-machine create --driver virtualbox --engine-insecure-registry 193.174.205.28:443 $vm_name
 echo "Changing settings to VM's docker"
-eval $(docker-machine env parcel-loadbalancer)
+eval $(docker-machine env $vm_name)
+echo "Creating network for containers"
+docker network create --driver bridge parcelnetwork
 echo "Starting containers"
-docker run -d --restart always --network=host --name=parcel-proxy 193.174.205.28:443/parcel-proxy
+docker run -d --restart always --network=parcelnetwork --name=parcel-db 193.174.205.28:443/parcel-db
+docker run -d --restart always --network=parcelnetwork -p 80:80 --name=parcel-frontend 193.174.205.28:443/parcel-frontend
+docker run -d --restart always --network=parcelnetwork --name=parcel-webservice1 193.174.205.28:443/parcel-api
+docker run -d --restart always --network=parcelnetwork --name=parcel-webservice2 193.174.205.28:443/parcel-api
+docker run -d --restart always --network=parcelnetwork -p 8443:8443 --name=parcel-proxy 193.174.205.28:443/parcel-proxy
 echo "Reset to host's docker"
 eval $(docker-machine env -u)
-echo "Installing socat"
-echo "tce-load -wi socat.tcz" | docker-machine ssh $vm_name
-echo "Setting IPs for backend servers"
-changeIpScript=$(cat changeProxyIp.sh)
-test_server1_url=$(docker-machine url $test_server1_name | grep -oP "tcp://\K[^:]+")
-test_server2_url=$(docker-machine url $test_server2_name | grep -oP "tcp://\K[^:]+")
-echo "vm_name=$test_server1_name &&\ vm_url=$test_server1_url &&\ $changeIpScript" | docker-machine ssh $vm_name
-echo "vm_name=$test_server2_name &&\ vm_url=$test_server2_url &&\ $changeIpScript" | docker-machine ssh $vm_name
+#echo "Installing socat"
+#echo "tce-load -wi socat.tcz" | docker-machine ssh $vm_name
+#echo "Setting IPs for backend servers"
+#changeIpScript=$(cat changeProxyIp.sh)
+#test_server1_url=$(docker-machine url $test_server1_name | grep -oP "tcp://\K[^:]+")
+#test_server2_url=$(docker-machine url $test_server2_name | grep -oP "tcp://\K[^:]+")
+#echo "vm_name=$test_server1_name &&\ vm_url=$test_server1_url &&\ $changeIpScript" | docker-machine ssh $vm_name
+#echo "vm_name=$test_server2_name &&\ vm_url=$test_server2_url &&\ $changeIpScript" | docker-machine ssh $vm_name
 echo "Loadbalancer's URL"
 echo $(docker-machine url $vm_name | grep -oP "tcp://\K[^:]+")
