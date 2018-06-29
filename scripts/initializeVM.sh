4ba17fbe8af6a24e9ab095ac9b20705887ddc2b0
comedian780 <aaron.s.luellwitz@stud.hs-kempten.de>
2018-06-29 07:58:04 +0200

    Dummheit ist unpraktisch

--- scripts/initializeVM.sh
+++ scripts/initializeVM.sh
@@ -8,15 +8,15 @@ if(docker-machine ls -q | grep "^$vm_name\$"); then
   docker-machine rm $vm_name -y
 fi
 echo "Creating VM for $vm_name"
-docker-machine create --driver virtualbox --engine-insecure-registry www.allgaeu-parcel-service.com:443 $vm_name
+docker-machine create --driver virtualbox --engine-insecure-registry asset.allgaeu-parcel-service.com:443 $vm_name
 echo "Changing settings to VM's docker"
 eval $(docker-machine env $vm_name)
 echo "Creating network for containers"
 docker network create --driver bridge parcelnetwork
 echo "Starting containers"
-docker run -d --restart always --network=parcelnetwork -p 3306:3306 --name=parcel-db www.allgaeu-parcel-service.com:443/parcel-db
-docker run -d --restart always --network=parcelnetwork -p 80:80 --name=parcel-frontend www.allgaeu-parcel-service.com:443/parcel-frontend
-docker run -d --restart always --network=parcelnetwork --name=parcel-webservice -p 8443:8443 www.allgaeu-parcel-service.com:443/parcel-api
+docker run -d --restart always --network=parcelnetwork -p 3306:3306 --name=parcel-db asset.allgaeu-parcel-service.com:443/parcel-db
+docker run -d --restart always --network=parcelnetwork -p 80:80 --name=parcel-frontend asset.allgaeu-parcel-service.com:443/parcel-frontend
+docker run -d --restart always --network=parcelnetwork --name=parcel-webservice -p 8443:8443 asset.allgaeu-parcel-service.com:443/parcel-api
 echo "Reset to host's docker"
 eval $(docker-machine env -u)
 echo "VM URL"
